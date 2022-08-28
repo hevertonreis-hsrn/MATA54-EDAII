387016ac10289b221dd09e44032761143c9adbb7
Heverton Reis <heverton.reis04@gmail.com>
2022-08-28 17:17:10 -0300

    doc: Separando Gravação e Leitura em funções

--- FileManager/main.c
+++ FileManager/main.c
@@ -1,13 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
-#define TAMARQUIVO 10
-#define TAMNOME 20
+#define MTAMARQUIVO 10
 
 typedef struct {
-	int cpf;
-	char nome[TAMNOME];
+	int chave;
 } Dados;
+
+typedef struct {
+	bool ocupado;
+	Dados dados;
+} Registro;
+
+int lerArquivo (FILE *file){
+
+	Registro registro;
+    int posicao;
+
+	for (int i=0; i < MTAMARQUIVO; i++) {
+
+		fread (&registro, sizeof (Dados), 1, file);
+
+		printf ("Registro: %d\n", i);
+
+		if (registro.ocupado == true) {
+
+			printf ("Ocupado: sim\n");
+			printf ("Chave: %d\n", registro.dados.chave);
+
+		} else
+
+			printf ("Ocupado: nao\n");
+
+		printf("\n");
+	}
+}
+
+int gravarArquivo(FILE *file){
+
+	Registro registro;
+	registro.ocupado = false;
+    int posicao;
+	
+	printf("Informe um valor para a chave (apenas numeros): ");
+	scanf("%d",&(registro.dados.chave));
+	printf("Informe a posicao no arquivo onde deseja inserir os dados: ");
+	scanf("%d",&posicao);
+
+    printf("Armazenando registro no arquivo ...\n");
+	registro.ocupado = true;
+
+	fseek(file, posicao*sizeof(Registro), SEEK_SET);
+
+	if (fwrite (&registro, sizeof(Registro), 1, file) == 1)
+		printf("Registro armazenado com sucesso\n");
+	else
+		printf("Erro no armazenamento do registro\n");
+}
 
 int main() {
 
@@ -20,22 +70,25 @@ int main() {
 		exit(-1);
     }
 
-    Dados registro;
+	//gravarArquivo(file);
+	lerArquivo(file);
+/*
+    Registro registro;
+	registro.ocupado = false;
     int posicao;
 	
-	printf("Informe um valor para o CPF (apenas numeros): ");
-	scanf("%d",&(registro.cpf));
-	printf("Informe um valor para o Nome (maximo de 20 caracteres): ");
-	scanf("%s", registro.nome);
-	printf("Informe a posicao do arquivo onde deseja inserir os dados: ");
+	printf("Informe um valor para a chave (apenas numeros): ");
+	scanf("%d",&(registro.dados.chave));
+	printf("Informe a posicao no arquivo onde deseja inserir os dados: ");
 	scanf("%d",&posicao);
 
     printf("Armazenando registro no arquivo ...\n");
-	fseek(file, posicao*sizeof(Dados), SEEK_SET);	
+	fseek(file, posicao*sizeof(Dados), SEEK_SET);
+		
 	if (fwrite(&registro, sizeof(Dados), 1, file) == 1)
 		printf("Registro armazenado com sucesso\n");
 	else
 		printf("Erro no armazenamento do registro\n");
-
+*/
 	fclose(file);
 }
